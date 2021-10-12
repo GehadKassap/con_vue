9995092492c7df8fb3b80ca9b7dee00e3c2b466d
Gehad Kassap <63156005+GehadKassap@users.noreply.github.com>
2021-10-12 16:26:46 +0200

    make http request through axios package

---
+++ src/components/Post.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <p v-if="errorMessage">{{errorMessage}}</p>
+       <button @click="getPosts">Load Posts</button>
+       <div v-for="post in posts" :key="post.id">
+           <h1>{{post.title}}</h1>
+                      <p>{{post.body}}</p>
+
+           <span>{{post.id}}</span>
+           <hr>
+
+       </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+    export default {
+        name:"Post",
+        data()
+        {
+            return {
+                posts : [],
+                errorMessage:'',
+            }
+        },
+        methods :
+        {
+             getPosts()
+             {
+               axios.get("https://jsonplaceholder.typicode.com/posts").then((response)=>{
+                   console.log(response.data)
+                   this.posts =  response.data;
+
+               }).catch((error)=>{
+                   console.log(error)
+                   this.errorMessage = "Eroor in Api get Request"
+                   })
+
+
+             }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
